4ec25c485430a3385e1fe6766ce276eb2b0734bc
Shilin Victor <chrono.monochrome@gmail.com>
2017-11-06 03:23:40 +0300

    Fix
    Change-Id: I30748fc6a631c9f4bf740eef6792947d629393c9

--- Android.mk
+++ Android.mk
@@ -8,6 +8,7 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 
 LOCAL_PACKAGE_NAME := TelephonyProvider
+LOCAL_DEX_PREOPT := false
 LOCAL_CERTIFICATE := platform
 
 LOCAL_JAVA_LIBRARIES += telephony-common
@@ -15,4 +16,4 @@ LOCAL_STATIC_JAVA_LIBRARIES += android-common
 
 include $(BUILD_PACKAGE)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+#include $(call all-makefiles-under,$(LOCAL_PATH))
